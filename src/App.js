37ff09a3b5fb7bde3513ab1a6da3242d0dbfda30
Alexey Kiselev <alexkisdev@yandex.ru>
2019-10-06 14:36:22 +0500

    43

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 
 function App(props) {
@@ -13,8 +13,8 @@ function App(props) {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/dialogs' render={() => <Dialogs store={props.store} />} />
-          <Route path='/profile' render={() => <Profile profilePage={props.state.profilePage} dispatch={props.dispatch} />} />
+          <Route path='/dialogs' render={() => <DialogsContainer store={props.store} />} />
+          <Route path='/profile' render={() => <Profile store = {props.store}  />} />
         </div>
       </div>
   );
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,26 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+  dialogs: [
+    { id: 1, name: 'Alexey' },
+    { id: 2, name: 'Victor' },
+    { id: 3, name: 'Oleg' },
+    { id: 4, name: 'Sasha' },
+    { id: 5, name: 'Yana' },
+    { id: 6, name: 'Pavel' }
+  ],
+  messages: [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Yo" },
+    { id: 4, message: "Yo" },
+    { id: 5, message: "Yo" }
+  ],
+  newMessageBody: ''
+}
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       state.newMessageBody = action.body;
